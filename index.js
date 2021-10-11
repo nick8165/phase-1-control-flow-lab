215610521ee8731b1a86a18df4e38cb68d34b99e
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-10-11 12:43:46 -0500

    commit tree 3845affb8d3ea26a51b00a6ef957dafe61e5e8c8

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet <= 400) {
+     return 'This one is on me!';
+  } else if (distanceInFeet > 2500) {
+      return 'No can do.';
+  } else if (distanceInFeet > 2000) {
+      return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ return city === 'NYC'? 'Ok, sounds good.':'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'Bye':
+      return 'thanks for everything';
+    default:
+      return 'Bye.';
+  }
 }
+
+let distanceInFeet = 0;
+let city = '';
+let tip = '';
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Houston'));
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('Bye'));
+console.log(switchOnCharmFromTip());
